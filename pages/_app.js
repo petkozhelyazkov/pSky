5546cd3527fe4ae34e62bf7e0716a2907dacf51a
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2022-05-02 22:43:08 +0300

    Create SearchFormComponent

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
+import Navigation from '../components/navigation/Navigation';
+import { UserState } from '../contexts/userState';
+import Footer from '../components/footer/Footer';
+import DateAdapter from '@mui/lab/AdapterMoment';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>pSky</title>
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css" />
+			</Head>
+			<LocalizationProvider dateAdapter={DateAdapter}>
+				<UserState>
+					<Navigation />
+					<Component {...pageProps} />
+				</UserState>
+			</LocalizationProvider>
+			<footer>
+				<Footer />
+			</footer>
+		</>
+	)
 }
 
 export default MyApp
