41eafc5795d9397d5226d9f9ee6035ad779202b3
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2024-07-09 12:53:23 +0300

    Uppdate redis URL

--- pages/api/createIndex.js
+++ pages/api/createIndex.js
@@ -2,5 +2,5 @@ import { createIndex } from "./airports/autocomplete";
 
 export default async function handler(req, res) {
     await createIndex();
-    res.status(200).send('ok');
+    res.status(200).send('ok')
 }
